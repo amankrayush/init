7affa21ab19a23f2ec7300d2914c8fcc12fcef6f
yenugukeerthana <96348559+yenugukeerthana@users.noreply.github.com>
2022-09-29 16:27:57 +0530

    Update and rename test.tsx to test.py

---
+++ .github/workflows/test.py
@@ -0,0 +1,8 @@
+
+# console.log(Comment.Changes.ResourceRecordSet.ResourceRecords.Value)
+import json
+fp=open("example.json")
+data=json.load(fp)
+print("Here is the data whic we have imported\n",data)
+#  Use the slicing to get a particular value from a json file.
+print("First Print")
